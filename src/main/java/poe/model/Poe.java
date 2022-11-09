d14533f73974a9867f2d9c67c0eb94804bd034eb
anthony.andre <anthony-andre@live.fr>
2022-11-09 10:34:29 +0100

    ADD: lombok and use of it

--- src/main/java/poe/model/Poe.java
+++ src/main/java/poe/model/Poe.java
@@ -1,55 +1,23 @@
 package poe.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter @Getter
 public class Poe {
-
-    public Poe() {
-    }
-
-    public Poe(String title, LocalDate beginDate, LocalDate endDate, PoeType poeType) {
-        this.title = title;
-        this.beginDate = beginDate;
-        this.endDate = endDate;
-        this.poeType = poeType;
-    }
 
     private String title;
     private LocalDate beginDate;
     private LocalDate endDate;
     private PoeType poeType;
 
-    public String getTitle() {
-        return title;
-    }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public LocalDate getBeginDate() {
-        return beginDate;
-    }
-
-    public void setBeginDate(LocalDate beginDate) {
-        this.beginDate = beginDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    public PoeType getPoeType() {
-        return poeType;
-    }
-
-    public void setPoeType(PoeType poeType) {
-        this.poeType = poeType;
-    }
 
     @Override
     public String toString() {
